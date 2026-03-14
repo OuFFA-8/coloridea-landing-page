c4ee190f0144570276126fcd73650219afbda10f
Yusef Alaa <ouffaalaa26@gmail.com>
2026-03-14 12:21:13 +0200

    refactor: update clients component structure and styles for improved layout and responsiveness

--- src/app/shared/components/clients/clients.ts
+++ src/app/shared/components/clients/clients.ts
@@ -1,22 +1,17 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-clients',
   standalone: true,
-  imports: [CommonModule],
   templateUrl: './clients.html',
   styleUrl: './clients.css',
 })
 export class Clients {
-  clients = [
-    'أسس العقارية',
-    'الزاملية',
-    'جادة المشاريع',
-    'أسس العقارية',
-    'الزاملية',
-    'جادة المشاريع',
-    'أسس العقارية',
-    'الزاملية',
+  logos = [
+    { src: 'assets/clients/osos.png', alt: 'أسس العقارية' },
+    { src: '/شعارات العملاء_لوقو الزاملية.png', alt: 'الزاملية' },
+    { src: '/شعارات العملاء_لوقو جادو.png', alt: 'جادة المشاريع' },
+    { src: 'assets/clients/majedia.png', alt: 'الماجدية' },
+    // أضف المزيد هنا بنفس الشكل
   ];
 }
